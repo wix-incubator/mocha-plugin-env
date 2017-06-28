bd4782b9465155ec4fc687b82bb1de4055faf9d8
amir-arad <amira@wix.com>
2017-06-28 15:51:35 +0300

    fixing bug when loaded before mocha

--- src/assuming.ts
+++ src/assuming.ts
@@ -1,28 +1,30 @@
-
-
 export type Context = {
-    assuming:(condition:boolean, message?:string) => Context;
-    skip:(expectation: string, callback?: Function)=> void;
+    assuming: (condition: boolean, message?: string) => Context;
+    skip: (expectation: string, callback?: Function) => void;
 } & (Mocha.IContextDefinition | Mocha.ITestDefinition);
 
 
 export const extend = (ctx: Context) => {
-    ctx.assuming = function assuming(condition:boolean, message:string = 'assumption not met'){
-        function skipped(description: string, callback: (...args:any[]) => void){
-            ctx.skip(`${description} (${message})`, callback);
-        }
-        if (condition){
-            return this;
-        } else {
-            (skipped as any).__proto__ = ctx;
-            return skipped as Context;
-        }
-    };
+    if (ctx) {
+        ctx.assuming = function assuming(condition: boolean, message: string = 'assumption not met') {
+            function skipped(description: string, callback: (...args: any[]) => void) {
+                ctx.skip(`${description} (${message})`, callback);
+            }
+            if (condition) {
+                return this;
+            } else {
+                (skipped as any).__proto__ = ctx;
+                return skipped as Context;
+            }
+        };
+    }
 };
 
 
 export const extendEmpty = (ctx: Context) => {
-    ctx.assuming = function assuming(condition:boolean, message:string = 'assumption not met'){
-        return this;
-    };
+    if (ctx) {
+        ctx.assuming = function assuming(condition: boolean, message: string = 'assumption not met') {
+            return this;
+        };
+    }
 };
